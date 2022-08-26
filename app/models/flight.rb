b44d82701ec1c50a12874d95469fcbdff26a5821
norkitorki <norki@tuta.io>
2022-08-26 11:48:23 +0200

    Create one-to-many relationship between airport and flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,9 @@
 class Flight < ApplicationRecord
   belongs_to :airline
+
+  belongs_to :departure_airport,
+    class_name: 'Airport'
+
+  belongs_to :arrival_airport,
+    class_name: 'Airport'
 end
