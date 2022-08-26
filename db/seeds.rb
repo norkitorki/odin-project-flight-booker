839c3d4c096f7aca051af8e62bb537f9aa2962b6
norkitorki <norki@tuta.io>
2022-08-26 11:32:51 +0200

    Seed the database with some airports and airlines

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Delete all entries from tables
+Airport.delete_all
+Airline.delete_all
+
+# Create some airports
+Airport.create(
+  [
+    { name: 'Oslo Gardemoen', city: 'Oslo', country: 'Norway' },
+    { name: 'Frankfurt am Main', city: 'Frankfurt', country: 'Germany' },
+    { name: 'Heathrow', city: 'London', country: 'United Kingdom' },
+    { name: 'Arlanda', city: 'Stockholm', country: 'Sweden' },
+    { name: 'Charles de Gaulle', city: 'Paris', country: 'France' },
+    { name: 'Leonardo da Vinci–Fiumicino', city: 'Rome', country: 'Italy' }
+  ]
+)
+
+# Create some airlines
+Airline.create(
+  [
+    { name: 'Lufthansa' },
+    { name: 'Norwegian' },
+    { name: 'Scandinavian Airlines' },
+    { name: 'British Airways' },
+    { name: 'Germanwings' },
+    { name: 'Air France' }
+  ]
+)
