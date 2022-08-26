5209927ed46a55ba7541af6a4d5ab402cbfcbcf4
norkitorki <norki@tuta.io>
2022-08-26 11:54:49 +0200

    Add arrival_time column to flights table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_085934) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_095309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_085934) do
     t.datetime "updated_at", null: false
     t.bigint "departure_airport_id", null: false
     t.bigint "arrival_airport_id", null: false
+    t.datetime "arrival_time"
     t.index ["airline_id"], name: "index_flights_on_airline_id"
     t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
