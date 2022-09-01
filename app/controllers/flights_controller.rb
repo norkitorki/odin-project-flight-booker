d591e78873fc90d99a2851b3b39545686378e82f
norkitorki <norki@tuta.io>
2022-09-01 11:05:35 +0200

    Remove search action and define index action helper methods

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,6 +5,32 @@ class FlightsController < ApplicationController
     @flights = flights_query(params) if params[:commit]
   end
 
-  def search
+  private
+
+  def airports_with_departures
+    Airport.includes(:departures).map { |a| a.departures.any? ? [a.city, a.id] : nil }.compact
+  end
+
+  def collect_departures
+    Airport.includes(:departures, :arrivals).select { |a| a.departures.any? }.map do |a|
+      destinations = a.departures.pluck(:arrival_airport_id).uniq.map do |id|
+        destination = Airport.find(id)
+        { id: destination.id,
+          city: destination.city,
+          departure_dates: destination.arrivals.where(departure_airport_id: a.id).pluck(:departure_time)
+        }
+      end
+
+      { id: a.id, city: a.city, destinations: destinations }
+    end
+  end
+
+  def flights_query(flight_params)
+    d = flight_params[:departure_airport]
+    a = flight_params[:arrival_airport]
+    t = Time.parse(flight_params[:departure_day].split('/').reverse.join('-'))
+
+    query = 'departure_time BETWEEN ? AND ? AND departure_airport_id = ? AND arrival_airport_id = ?'
+    Flight.includes(:airline).where(query, t.midnight, t.end_of_day, d, a)
   end
 end
