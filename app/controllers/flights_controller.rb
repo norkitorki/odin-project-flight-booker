fccd4cc18ff7c862ffc9cb1ad701e8a98866fa76
norkitorki <norki@tuta.io>
2022-09-01 11:04:02 +0200

    Define necessary variables in index action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
   def index
+    @departure_airports = airports_with_departures
+    gon.departures = collect_departures
+    @flights = flights_query(params) if params[:commit]
   end
 
   def search
