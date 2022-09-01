960a22078fc0e5e0e2150b2cb2966c473158bbf5
norkitorki <norki@tuta.io>
2022-09-01 13:23:36 +0200

    Create a one-to-many relationship between airline and bookings,through flights

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,5 +1,12 @@
 class Airline < ApplicationRecord
   has_many :flights
-  has_many :departure_airports, through: :flights
-  has_many :arrival_airports, through: :flights
+
+  has_many :departure_airports, 
+    through: :flights
+
+  has_many :arrival_airports, 
+    through: :flights
+
+  has_many :bookings,
+    through: :flights
 end
