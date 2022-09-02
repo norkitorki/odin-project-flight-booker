2ac8d6ddf21c1fdcf9a20f3738cd057589efca66
norkitorki <norki@tuta.io>
2022-09-02 10:53:55 +0200

    Create one-to-many relationship with passengers thorugh bookings

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,5 +7,9 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport,
     class_name: 'Airport'
 
-  has_many :bookings
+  has_many :bookings,
+    dependent: :destroy
+
+  has_many :passengers,
+    through: :bookings
 end
