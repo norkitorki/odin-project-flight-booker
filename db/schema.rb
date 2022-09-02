ffbd1cb3720c194b0fef5eff817cebe22bef22de
norkitorki <norki@tuta.io>
2022-09-02 07:19:44 +0200

    Create passengers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_111930) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_02_051837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_111930) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.bigint "booking_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
+  end
+
   add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airlines"
+  add_foreign_key "passengers", "bookings"
 end
