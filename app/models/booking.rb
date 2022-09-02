1f8c6a3f6de89094e3191af30492f4e178ad8ca8
norkitorki <norki@tuta.io>
2022-09-02 10:53:21 +0200

    Accept nested passenger attributes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :flight
 
-  has_many :passengers
+  has_many :passengers,
+    dependent: :destroy
+
+  accepts_nested_attributes_for :passengers
 end
