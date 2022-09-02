914c8646eafde36219eb624200069796164f6753
norkitorki <norki@tuta.io>
2022-09-02 13:29:30 +0200

    Add allow_destroy option to nested passengers attribute

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,5 @@ class Booking < ApplicationRecord
   has_many :passengers,
     dependent: :destroy
 
-  accepts_nested_attributes_for :passengers
+  accepts_nested_attributes_for :passengers, allow_destroy: true
 end
