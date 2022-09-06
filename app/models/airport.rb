4e72b92aeff19920992cbdd737df11992494879a
norkitorki <norki@tuta.io>
2022-09-06 13:08:34 +0200

    Add dependent: :destroy option to flights association

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,9 +1,11 @@
 class Airport < ApplicationRecord
   has_many :departures,
     class_name: 'Flight',
-    foreign_key: :departure_airport_id
+    foreign_key: :departure_airport_id,
+    dependent: :destroy
 
   has_many :arrivals,
     class_name: 'Flight',
-    foreign_key: :arrival_airport_id
+    foreign_key: :arrival_airport_id,
+    dependent: :destroy
 end
