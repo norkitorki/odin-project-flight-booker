a48ee435a028e52c96614fd20a5678fbdfa0565f
norkitorki <norki@tuta.io>
2022-09-09 08:42:59 +0200

    Redirect to new_bookings_path on unsuccessfull save operation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -16,8 +16,9 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to booking_path(@booking), notice: 'Your booking has been successfully created.'
     else
-      flash.now[:alert] = 'Your booking as not been created.'
-      render :new, status: :unprocessable_entity
+      passenger_count = (params[:booking][:passengers_attributes].keys.last.to_i + 1)
+      redirect_to new_booking_path(params: { flight_id: @booking.flight_id, passenger_count: passenger_count }), 
+        alert: @booking.errors.full_messages.join(' and ') << '.'
     end
   end
 
