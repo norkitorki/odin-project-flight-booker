eb6224e7173d1c53d3a4090851f15643a9a35759
norkitorki <norki@tuta.io>
2022-09-12 13:08:47 +0200

    Create bookings_passengers join table and remove booking_id column from passengers table

---
+++ db/migrate/20220911061424_create_bookings_passengers_join_table.rb
@@ -0,0 +1,5 @@
+class CreateBookingsPassengersJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :bookings, :passengers
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_02_051837) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_11_061913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_051837) do
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
   end
 
+  create_table "bookings_passengers", id: false, force: :cascade do |t|
+    t.bigint "booking_id", null: false
+    t.bigint "passenger_id", null: false
+  end
+
   create_table "flights", force: :cascade do |t|
     t.datetime "departure_time"
     t.bigint "airline_id", null: false
@@ -51,13 +56,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_02_051837) do
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.bigint "booking_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
   add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airlines"
-  add_foreign_key "passengers", "bookings"
 end
