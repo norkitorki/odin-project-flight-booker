28cbb38024c5b3d93b9b977c07137bf9b628b44c
norkitorki <norki@tuta.io>
2022-09-13 07:15:03 +0200

    Add before_validation callback to validate if the passenger is already on the flight list

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,24 @@
 class Booking < ApplicationRecord
+  before_validation :validate_passenger_existence
   belongs_to :flight
 
   has_and_belongs_to_many :passengers
 
-  validates :passengers, 
+  validates :passengers,
     presence: true
 
   accepts_nested_attributes_for :passengers, 
     allow_destroy: true,
     reject_if: :all_blank
+
+  private
+
+  def validate_passenger_list_presence
+    flight_list = flight.passengers
+    passengers.each do |passenger|
+      if flight_list.any? { |p| p.name == passenger.name && p.email == passenger.email }
+        errors.add(:passenger, "#{passenger.name} is already on the flight list")
+      end
+    end
+  end
 end
